f0eec3fac207ea38bc5cc0788aed46a3d9d341b9
sitri <wc348nka@gmail.com>
2016-10-14 20:25:50 +0900

    jenvとluaenvを追加

--- install_middleware.sh
+++ install_middleware.sh
@@ -3,6 +3,8 @@ git clone https://github.com/riywo/anyenv ~/.anyenv
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
 exec $SHELL -l
+anyenv install jenv
+anyenv install luaenv
 anyenv install nodenv
 anyenv install phpenv
 anyenv install pyenv
