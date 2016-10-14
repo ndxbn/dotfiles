1f17599afcc1f60e8a9bc575653cb4a8e1ccfc2d
sitri <wc348nka@gmail.com>
2016-10-14 20:26:13 +0900

    事前にyumでインストールしておくものを追加

--- install_middleware.sh
+++ install_middleware.sh
@@ -1,3 +1,58 @@
+yum update -y
+yum -y install epel-release
+yum -y install\
+  bind-utils\
+  bzip2-devel\
+  curl-devel\
+  cronie-noanacron\
+  cmake3\
+  expat-devel\
+  gcc\
+  gcc-c++\
+  gettext-devel\
+  git\
+  gmp-devel\
+  graphviz\
+  kernel-devel\
+  libaio-devel\
+  libcurl-devel\
+  libedit\
+  libedit-devel\
+  libevent\
+  libevent-devel\
+  libffi-devel\
+  libjpeg-devel\
+  libpng-devel\
+  libtool\
+  libtool-ltdl-devel\
+  libxml2-devel\
+  icu\
+  make\
+  monit\
+  monitcronie\
+  monitpostfix\
+  ncurses-devel\
+  openssl-devel\
+  pcre\
+  pcre-devel\
+  perl\
+  postfix\
+  python-docutils\
+  python34\
+  readline-devel\
+  sqlite-devel\
+  tk-devel\
+  unzip\
+  wget\
+  which\
+  xz\
+  xz-devel\
+  xz-libs\
+  zlib-devel\
+  zip
+
+ln -s /usr/bin/cmake3 /usr/local/bin/cmake
+
 # any env
 git clone https://github.com/riywo/anyenv ~/.anyenv
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
