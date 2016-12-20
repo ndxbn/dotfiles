a7a357899c5eeed8e9ce9713f02086fc2bff160a
sitri <wc348nka@gmail.com>
2016-12-20 15:33:43 +0900

    move /install.sh /docs
    github pages を使って、 `install.sh` へアクセスできるように、ディレクトリ構造を変更した。

--- docs/install.sh
+++ docs/install.sh
@@ -1,9 +1,11 @@
-if ! which git >& /dev/null
-then
+#!/usr/bin/env bash
+if ! hash git >& /dev/null; then
   logger -s git command was not found.
   exit 1
 fi
 
 cd ~
+mkdir ndxbn && cd ndxbn
 git clone https://github.com/ndxbn/dotfiles.git
 cd dotfiles
+bash install.sh
