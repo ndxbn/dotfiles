e7430b73a968acadbae7088857c10de5efef4455
ndxbn <wc348nka@gmail.com>
2017-08-24 20:42:16 +0900

    ディレクトリ構造を、 GNU Stow の形式に変更

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ue
-git clone http://github.com/ndxbn/dotfiles ~/dotfiles
-cd ~/dotfiles
-rm -rf install.sh LICENSE README.md .gitignore .git
-rsync -avh --no-perms . ~
-source ~/.bash_profile
