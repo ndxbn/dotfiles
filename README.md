69c55194d6acb709a9f5e841aad4dc29e49ed398
ndxbn <kota_suzuki@dwango.co.jp>
2018-04-15 02:25:51 +0900

    add mac.sh

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 [GNU Stow](https://www.gnu.org/software/stow/) を使用する。
 
+## docker のインストールについて
+`brew install docker` は、`docker` コマンドのインストールであって、 `dockerd` のインストールではない。
+`dockerd` は Homebrew Cask でインストーラを取ってきて、 `/Application/Docker.app` でインストールとセットアップする。
+
+
---
+++ mac.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eu
+
+if [ -e ~/.local/dotfiles ]; then
+  echo You have run dotfile installer yet.
+  exit 1
+fi
+
+mkdir -p ~/.local && touch ~/.local/dotfiles
+
+# install Home Brew
+# https://brew.sh/
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install via brew
+brew install \
+  curl \
+  docker \
+  docker-compose
+  tmux \
+  wget \
+  zsh
+
+brew cask install \
+  docker
+
+# install zplug
+export ZPLUG_HOME=~/.zplug
+git clone https://github.com/zplug/zplug ${ZPLUG_HOME}
+
