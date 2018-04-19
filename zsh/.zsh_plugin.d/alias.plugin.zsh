2b5fa56e1aa16daec720d170dfb948b1ef443aa7
ndxbn <kota_suzuki@dwango.co.jp>
2018-04-19 21:28:46 +0000

    update zsh plugin somethings

--- zsh/.zsh_plugin.d/alias.plugin.zsh
+++ zsh/.zsh_plugin.d/alias.plugin.zsh
@@ -1,4 +1,4 @@
-# ls
+# ls family
 alias ls='ls --color=auto'
 alias la='ls -A'
 alias ll='ls -l'
@@ -7,6 +7,7 @@ alias lla='ls -Al'
 # shorthands
 alias zshrc='$EDITOR ~/.zshrc'
 alias t='tail -f'
+alias zplugclean='zplug clear && zplug clean'
 
 # default options
 alias grep='grep --color'
--- zsh/.zsh_plugin.d/bindkey.plugin.zsh
+++ zsh/.zsh_plugin.d/bindkey.plugin.zsh
@@ -1,3 +1,40 @@
-bindkey "${terminfo[khome]}" beginning-of-line
-bindkey "${terminfo[kend]}" end-of-line
+# create a zkbd compatible hash;
+# to add other keys to this hash, see: man 5 terminfo
+typeset -A key
+
+## Navigation Keys
+key[Insert]=${terminfo[kich1]}
+key[Delete]=${terminfo[kdch1]}
+key[Home]=${terminfo[khome]}
+key[End]=${terminfo[kend]}
+key[PageUp]=${terminfo[kpp]}
+key[PageDown]=${terminfo[knp]}
+
+key[Up]=${terminfo[kcuu1]}
+key[Down]=${terminfo[kcud1]}
+key[Left]=${terminfo[kcub1]}
+key[Right]=${terminfo[kcuf1]}
+
+[[ -n "${key[Insert]}"   ]]  && bindkey  "${key[Insert]}"   overwrite-mode
+[[ -n "${key[Delete]}"   ]]  && bindkey  "${key[Delete]}"   delete-char
+[[ -n "${key[Home]}"     ]]  && bindkey  "${key[Home]}"     beginning-of-line
+[[ -n "${key[End]}"      ]]  && bindkey  "${key[End]}"      end-of-line
+[[ -n "${key[PageUp]}"   ]]  && bindkey  "${key[PageUp]}"   beginning-of-buffer-or-history
+[[ -n "${key[PageDown]}" ]]  && bindkey  "${key[PageDown]}" end-of-buffer-or-history
+
+[[ -n "${key[Up]}"       ]]  && bindkey  "${key[Up]}"       up-line-or-history
+[[ -n "${key[Down]}"     ]]  && bindkey  "${key[Down]}"     down-line-or-history
+[[ -n "${key[Left]}"     ]]  && bindkey  "${key[Left]}"     backward-char
+[[ -n "${key[Right]}"    ]]  && bindkey  "${key[Right]}"    forward-char
+
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+    function zle-line-init () {
+        printf '%s' "${terminfo[smkx]}"
+    }
+    function zle-line-finish () {
+        printf '%s' "${terminfo[rmkx]}"
+    }
+    zle -N zle-line-init
+    zle -N zle-line-finish
+fi
 
