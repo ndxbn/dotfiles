449f986bc9b61ab882221e8ac573920aadd0ff0d
ndxbn <kota_suzuki@dwango.co.jp>
2018-05-08 05:42:08 +0900

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -67,7 +67,7 @@ sudo yum install -y \
   yum-utils \
   device-mapper-persistent-data \
   lvm2
-sudo yum-config-manager--add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum -y install docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
