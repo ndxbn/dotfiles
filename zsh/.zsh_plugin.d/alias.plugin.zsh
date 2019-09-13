b90f2d819b230b949b8fd1d806c72ba417fe8373
ndxbn <sitri.ndxbn@gmail.com>
2019-09-13 03:55:44 +0000

    add "vim" command alias

--- zsh/.zsh_plugin.d/alias.plugin.zsh
+++ zsh/.zsh_plugin.d/alias.plugin.zsh
@@ -7,5 +7,8 @@ alias lla='ls -Al'
 # default options
 alias grep='grep --color'
 
+# another name
+alias vim='vi'
+
 # shorthand
 alias http-server='docker run -v `pwd`:/usr/share/nginx/html:ro  -p 8080:80 nginx:alpine'
