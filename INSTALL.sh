bdba61a261a4f5f50ffcf0daf9456811b56ce810
ndxbn <sitri.ndxbn@gmail.com>
2019-11-08 19:02:45 +0900

    fix lint error

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,12 +4,12 @@ set -eu
 # comman exists?
 type stow
 
-stow */
+stow ./*/
 
 # install ssh-config
 ## make .ssh directory if not exist
-[[ -d ${HOME}/.ssh ]] || mkdir ${HOME}/.ssh && chmod 700 ${HOME}/.ssh 
+[[ -d ${HOME}/.ssh ]] || mkdir "${HOME}/.ssh" && chmod 700 "${HOME}/.ssh"
 ## copy ssh-config if not exist
-[[ -f ${HOME}/.ssh/config ]] || cp ssh-config ${HOME}/.ssh/config && chmod 600 ${HOME}/.ssh/config
+[[ -f ${HOME}/.ssh/config ]] || cp ssh-config "${HOME}/.ssh/config" && chmod 600 "${HOME}/.ssh/config"
 ## touch empty local config if not exist
-[[ -f ${HOME}/.ssh/config-local ]] || touch ${HOME}/.ssh/config-local && chmod 600 ${HOME}/.ssh/config-local
+[[ -f ${HOME}/.ssh/config-local ]] || touch "${HOME}/.ssh/config-local" && chmod 600 "${HOME}/.ssh/config-local"
