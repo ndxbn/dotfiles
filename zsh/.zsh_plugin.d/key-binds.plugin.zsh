348f459ba3f363814ff8e5717846baba1cd2bfc3
ndxbn <sitri.ndxbn@gmail.com>
2021-11-05 13:33:47 +0900

    fix Home/End key bind bug and add some key bind

--- zsh/.zsh_plugin.d/key-binds.plugin.zsh
+++ zsh/.zsh_plugin.d/key-binds.plugin.zsh
@@ -1,5 +1,40 @@
 tic ~/terminfo.src
+bindkey -e
 
-bindkey "${terminfo[khome]}" beginning-of-line
-bindkey "${terminfo[kend]}" end-of-line
+# [Ctrl-u]
+bindkey "^U" backward-kill-line
+## ${terminfo[khome]} is "^[0H", and it makes bug
+# [Home]
+bindkey "^[[H" beginning-of-line
+## ${terminfo[kend]} is "^[0F", and it makes bug
+# [End]
+bindkey  "^[[F"   end-of-line
 
+# [Shift-Tab]
+if [[ -n "${terminfo[kcbt]}" ]]; then
+  bindkey -M emacs "${terminfo[kcbt]}" reverse-menu-complete
+  bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
+  bindkey -M vicmd "${terminfo[kcbt]}" reverse-menu-complete
+fi
+if [[ -n "${terminfo[kdch1]}" ]]; then
+  bindkey -M emacs "${terminfo[kdch1]}" delete-char
+  bindkey -M viins "${terminfo[kdch1]}" delete-char
+  bindkey -M vicmd "${terminfo[kdch1]}" delete-char
+else
+  bindkey -M emacs "^[[3~" delete-char
+  bindkey -M viins "^[[3~" delete-char
+  bindkey -M vicmd "^[[3~" delete-char
+
+  bindkey -M emacs "^[3;5~" delete-char
+  bindkey -M viins "^[3;5~" delete-char
+  bindkey -M vicmd "^[3;5~" delete-char
+fi
+
+# [Ctrl-RightArrow]
+bindkey -M emacs '^[[1;5C' forward-word
+bindkey -M viins '^[[1;5C' forward-word
+bindkey -M vicmd '^[[1;5C' forward-word
+# [Ctrl-LeftArrow]
+bindkey -M emacs '^[[1;5D' backward-word
+bindkey -M viins '^[[1;5D' backward-word
+bindkey -M vicmd '^[[1;5D' backward-word
