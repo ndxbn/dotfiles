4e5e2dd02d34062e3eedc9e8fc793bbc44fc73cd
ndxbn <sitri.ndxbn@gmail.com>
2024-10-28 00:49:42 +0900

    chore: update some alias

--- zsh/.zsh_plugin.d/alias.plugin.zsh
+++ zsh/.zsh_plugin.d/alias.plugin.zsh
@@ -12,9 +12,10 @@ alias vim='vi'
 
 # shorthand
 alias http-server='docker run -v `pwd`:/usr/share/nginx/html:ro  -p 8080:80 nginx:alpine'
-alias tmp='pushd $(TMPDIR=${HOME}/.local/tmp mktemp -d)'
+alias tmp='pushd "$(TMPDIR=${HOME}/.local/tmp mktemp -d)"'
 
 # GitHub tools
-alias myrepo='gh repo list --json name --jq ".[].name" --no-archived | peco | xargs ghq get'
-alias cdrepo='pushd $(ghq root)/$(ghq list | peco)'
+alias myrepo='gh repo list --json name --jq ".[].name" --no-archived | peco | xargs ghq get -p'
+## "cd repo" short name
+alias cdpo='pushd "$(ghq root)/$(ghq list | peco)"'
 
