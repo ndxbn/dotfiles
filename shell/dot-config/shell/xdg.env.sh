bed8d5ed1ffdcd6e22419bbed4bb2c083d5a4cae
Sitri Kamishirasawa <wc348nka@gmail.com>
2025-12-02 18:04:48 +0900

    feat: dirty hacking to fix ci envs
    CI環境だと一部の環境変数だけ設定されてるため、上書きしたほうが楽。

--- shell/dot-config/shell/xdg.env.sh
+++ shell/dot-config/shell/xdg.env.sh
@@ -1,11 +1,11 @@
 # shellcheck shell=bash
 ## XDG Base Directory
 ## https://specifications.freedesktop.org/basedir/latest
-export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
-export PATH="$HOME/.local/bin:$PATH"
-export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_STATE_HOME="${HOME}/.local/state"
+export PATH="${HOME}/.local/bin:${PATH}"
+export XDG_CACHE_HOME="${HOME}/.cache"
 
 # You SHOULD NOT set XDG_XXX_DIRS env vars. They MAY be set by OS level operation.
 
