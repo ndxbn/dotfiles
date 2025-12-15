50859a8055c1ce01c5f90a710f79a3b375f1e99d
Sitri Kamishirasawa <wc348nka@gmail.com>
2025-12-15 19:24:33 +0900

    feat: zsh directory hash
    ハッシュはCLIからしかつかわれないので、プラグインとして出しておき、
    スタートアップ処理から使用されないことを保証したい。

---
+++ zsh/dot-local/share/zsh/plugins/_local/hash.zsh
@@ -0,0 +1,8 @@
+hash -d data="${XDG_DATA_HOME}"
+hash -d config="${XDG_CONFIG_HOME}"
+hash -d state="${XDG_STATE_HOME}"
+hash -d cache="${XDG_CACHE_HOME}"
+
+hash -d debuglog="${DEBUG_LOG_DIR}"
+hash -d plug="${_ZPLUGIN_DIR}"
+
