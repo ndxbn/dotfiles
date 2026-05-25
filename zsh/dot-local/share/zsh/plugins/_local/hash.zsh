0b9571eb3115301803e1ad33fbd3e3165066b9cb
Sitri Kamishirasawa <wc348nka@gmail.com>
2026-05-25 18:23:38 +0900

    feat: useful zsh directory hash

--- zsh/dot-local/share/zsh/plugins/_local/hash.zsh
+++ zsh/dot-local/share/zsh/plugins/_local/hash.zsh
@@ -1,8 +1,17 @@
+# XDG Base Dirs
 hash -d data="${XDG_DATA_HOME}"
 hash -d config="${XDG_CONFIG_HOME}"
 hash -d state="${XDG_STATE_HOME}"
 hash -d cache="${XDG_CACHE_HOME}"
+## with "x" prefix
+hash -d xdata="${XDG_DATA_HOME}"
+hash -d xconfig="${XDG_CONFIG_HOME}"
+hash -d xstate="${XDG_STATE_HOME}"
+hash -d xcache="${XDG_CACHE_HOME}"
 
+# for zsh scripts
 hash -d debuglog="${DEBUG_LOG_DIR}"
 hash -d plug="${_ZPLUGIN_DIR}"
-
+## with "z" prefix
+hash -d zdebuglog="${DEBUG_LOG_DIR}"
+hash -d zplug="${_ZPLUGIN_DIR}"
